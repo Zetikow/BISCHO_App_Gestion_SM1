e656222db854efa38a148da7a44d6232b2dddb9a
Zetikow <maximilien.mrch@gmail.com>
2026-07-01 19:29:46 +0200

    Add files via upload

---
+++ google-script-config.js
@@ -0,0 +1,9 @@
+// ===================================================================
+// CONFIGURATION GOOGLE SHEETS
+// ===================================================================
+// Colle ici l'URL obtenue à l'étape 8 du fichier apps-script.js
+// Elle ressemble à :
+// https://script.google.com/macros/s/AKfycb.../exec
+// ===================================================================
+
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw_YjycCP7NA4GqBIy9M015L2HeRqgtnUPi2Pgk1Of4tnTv-VabBWnBLescM_XLoOaohg/exec";
