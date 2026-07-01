3d607c2feddd4404d56ff30b3e1013157c4db685
Zetikow <maximilien.mrch@gmail.com>
2026-07-01 19:28:22 +0200

    Delete caisse-app directory

--- caisse-app/google-script-config.js
+++
@@ -1,9 +0,0 @@
-// ===================================================================
-// CONFIGURATION GOOGLE SHEETS
-// ===================================================================
-// Colle ici l'URL obtenue à l'étape 8 du fichier apps-script.js
-// Elle ressemble à :
-// https://script.google.com/macros/s/AKfycb.../exec
-// ===================================================================
-
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw_YjycCP7NA4GqBIy9M015L2HeRqgtnUPi2Pgk1Of4tnTv-VabBWnBLescM_XLoOaohg/exec";
